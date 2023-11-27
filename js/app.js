17f3e7bc85ce85de5aba5c459007304ab5088352
viktor-rumiievskyi <vityok05042002@gmail.com>
2023-11-27 18:50:24 +0200

    I started writing the project again. I corrected the mistakes.

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var cursor = document.querySelector('.cursor'),
+    cursorScale = document.querySelectorAll('.cursor-scale'),
+    mouseX = 0,
+    mouseY = 0
+
+gsap.to({}, 0.016, {
+    repeat: -1,
+
+    onRepeat: function () {
+        gsap.set(cursor, {
+            css: {
+                left: mouseX,
+                top: mouseY
+            }
+        })
+    }
+});
+
+window.addEventListener("mousemove", function (e) {
+	mouseX = e.clientX;
+	mouseY = e.clientY
+});
